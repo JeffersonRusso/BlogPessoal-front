eea0894a437f214b43e8c4f19d43b76fe22ef6c7
Jefferson Russo Silva <jefferson.russo@Outlook.com>
2020-11-10 21:02:23 -0300

    Consumo API

--- BlogPessoal/src/app/feed/feed.component.ts
+++ BlogPessoal/src/app/feed/feed.component.ts
@@ -1,4 +1,8 @@
   import { Component, OnInit } from '@angular/core';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-feed',
@@ -7,10 +11,38 @@
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  postagem : Postagem = new Postagem()
+  listaPostagem: Postagem[]
+
+  tema: Tema = new Tema()
+  listaTema : Tema[]
+  idTema : number
+
+
+
+  constructor(
+    private PostagemService : PostagemService,
+    private temaService : TemaService
+  ) { }
+
+  findAllPostagem() {
+    this.PostagemService.getAllPostagem().subscribe((resp: Postagem[]) => {
+      this.listaPostagem = resp
+    })
+  }
+  
+  findAllTema() {
+    this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+      this.listaTema = resp
+    })
+  }
 
   ngOnInit(){
     window.scroll(0,0)
+
+    this.findAllPostagem()
+    this.findAllTema()
+
   }
 
 }
